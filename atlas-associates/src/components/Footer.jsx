356c844d3b081a5674eeaa19af2dd3df0809cebf
alexy-ok <alexanderyin5280@gmail.com>
2024-08-31 11:36:52 -0700

    footer updated with contact info

--- atlas-associates/src/components/Footer.jsx
+++ atlas-associates/src/components/Footer.jsx
@@ -10,6 +10,10 @@ function Footer() {
         <footer className={styles.footer}>
             <div className={styles.footerContent}>
                 <p>©Atlas Associates 2024. All rights reserved.</p>
+                <div className={styles.right}>
+                    <p>Phone: (206)488-3688</p>
+                    <p>Email: gengtan@gmail.com</p>
+                </div>
             </div>
         </footer>
         </>
